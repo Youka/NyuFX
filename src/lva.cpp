cfe34670defd9e7e346ed9831910fb3352ae0e0d
Youka <spanknebel.borken@t-online.de>
2013-01-29 13:22:19 +0100

    began Lua VideoAudio library

--- src/lva.cpp
+++ src/lva.cpp
@@ -1,13 +1,58 @@
 #include "llibs.h"
 #define __STDC_CONSTANT_MACROS 1
 extern "C"{
-	#include <inttypes.h>
-	#include <libavcodec/avcodec.h>
 	#include <libavformat/avformat.h>
-	#include <libavutil/avutil.h>
+	#include <libswscale/swscale.h>
 }
+#include <list>
+
+// UTILITIES
+#define DEMUXER "demuxer"
+#define MUXER "muxer"
+#define ISTREAM "istream"
+#define OSTREAM "ostream"
+
+struct VAStream;
+struct VAFormat{
+	AVFormatContext* format;
+	std::list<VAStream*> streams;
+};
+struct VAStream{
+	AVStream *stream;
+	AVFormatContext* format;
+	bool has_format;
+};
+
+// FUNCTIONS
 
 // REGISTER
-void luaopen_va(lua_State *L){
+inline void register_va_meta(lua_State *L){
+	// Initialize libav
 	av_register_all();
+	// Meta methods
+	luaL_newmetatable(L, DEMUXER);
+	lua_pushvalue(L, -1); lua_setfield(L, -2, "__index");
+	lua_pop(L, 1);
+	luaL_newmetatable(L, MUXER);
+	lua_pushvalue(L, -1); lua_setfield(L, -2, "__index");
+	lua_pop(L, 1);
+	luaL_newmetatable(L, ISTREAM);
+	lua_pushvalue(L, -1); lua_setfield(L, -2, "__index");
+	lua_pop(L, 1);
+	luaL_newmetatable(L, OSTREAM);
+	lua_pushvalue(L, -1); lua_setfield(L, -2, "__index");
+	lua_pop(L, 1);
+}
+
+inline void register_va_lib(lua_State *L){
+	const luaL_Reg va[] = {
+		{0, 0}
+	};
+	luaL_register(L, "va", va);
+	lua_pop(L, 1);
+}
+
+void luaopen_va(lua_State *L){
+	register_va_meta(L);
+	register_va_lib(L);
 }
