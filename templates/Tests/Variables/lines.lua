2878c1551791385d0d0f4791e6414e92297cb940
Youka <spanknebel.borken@t-online.de>
2013-02-08 07:10:06 +0100

    renamed audio template to better name; added lines test template; removed ASS tags document (outdated)

---
+++ templates/Tests/Variables/lines.lua
@@ -0,0 +1,5 @@
+io.load_ass([[
+Comment: 1,0:00:00.00,0:00:10.00,Default,,0000,0000,0000,,
+Dialogue: 0,0:00:12.00,0:00:20.00,girly,girl 1,0000,0000,0000,translation right?,{\k300}Fu{\k100}n{\k400}ya
+]])
+print(table.tostring(lines))
