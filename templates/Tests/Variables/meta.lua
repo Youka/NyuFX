ca2b9150ec45cd2a166a20500d77e4427878d3a9
Youka <spanknebel.borken@t-online.de>
2013-02-08 03:45:18 +0100

    began variables test

---
+++ templates/Tests/Variables/meta.lua
@@ -0,0 +1,16 @@
+io.load_ass([[
+; Created by Youka
+Title: Meta parsing test
+ScriptType: v4.00+
+WrapStyle: 0
+PlayResX: 1920
+PlayResY: 1080
+ScaledBorderAndShadow: yes
+Video Aspect Ratio: 0
+Video Zoom: 8
+Video Position: 0
+Last style Storage: Default
+Audio File: ?video
+Video File: ?dummy:25.000000:250:1280:1080:0:0:0:
+]])
+print(table.tostring(meta))
---
+++ templates/Tests/Variables/styles.lua
@@ -0,0 +1,5 @@
+io.load_ass([[
+Style: Speaker A,Tahoma,50,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,2,0,2,10,10,15,1
+Style: Speaker B,Arial,48,&HFFFFFFFF,&H000000FF,&H00000000,&H00000000,0,-1,0,0,100,100,0.5,0,1,1.5,1,8,10,10,20,1
+]])
+print(table.tostring(styles))
